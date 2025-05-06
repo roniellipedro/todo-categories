39d245db79af4b69a5608a5c7f833d21102f7c42
Ronielli <roniellipedrocontato@gmail.com>
2025-05-06 07:27:56 -0300

    inicio de criação no login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,10 @@
+<x-layout title="Todo - Login" btn="Registre-se">
+    <x-slot:btn>
+        <a href="" class="btn btn-primary">
+            Registre-se
+        </a>
+
+    </x-slot:btn>
+
+    Login
+</x-layout>
