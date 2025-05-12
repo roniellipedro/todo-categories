b9a024f1acccfb2760e6a9dd2362661d093b585d
Ronielli <roniellipedrocontato@gmail.com>
2025-05-11 23:14:54 -0300

    protegendo as rotas do usuário não autenticado

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,24 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/task/create', [TaskController::class, 'create'])->name('task.create');
-Route::post('/task/create_action', [TaskController::class, 'create_action'])->name('task.create_action');
+    Route::get('/task/create', [TaskController::class, 'create'])->name('task.create');
+    Route::post('/task/create_action', [TaskController::class, 'create_action'])->name('task.create_action');
 
-Route::get('/task/edit', [TaskController::class, 'edit'])->name('task.edit');
-Route::post('/task/edit_action', [TaskController::class, 'edit_action'])->name('task.edit_action');
+    Route::get('/task/edit', [TaskController::class, 'edit'])->name('task.edit');
+    Route::post('/task/edit_action', [TaskController::class, 'edit_action'])->name('task.edit_action');
 
-Route::get('/task/delete', [TaskController::class, 'delete'])->name('task.delete');
+    Route::get('/task/delete', [TaskController::class, 'delete'])->name('task.delete');
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'login_action'])->name('login_action');
 
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'register_action'])->name('register_action');
-
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
